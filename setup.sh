8f14d2ac8910f1decd04dcaa7badf3de9811a856
michael <16124@DESKTOP-5DR3TOT.MCOMPANY.local>
2022-05-22 12:31:49 -0500

    first deployment

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-mkdir -p ~/.streamlit/credentials.toml
+mkdir -p ~/.streamlit/
 
-echo "/
+echo "\
 [server]\n\
 port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
 \n\
-" > ~/.streamlit//config.toml
+" > ~/.streamlit/config.toml
