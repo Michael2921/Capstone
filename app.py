21972e9393cbadfc2266cf391a510697d0d6624a
michael <16124@DESKTOP-5DR3TOT.MCOMPANY.local>
2022-05-28 19:31:52 -0500

    first deployment

--- app.py
+++ app.py
@@ -5,23 +5,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 cartoon_data = pd.read_csv("cartoons.csv")
-# Removing extraneous columns
-cartoon_data.drop("Unnamed: 0", axis=1, inplace=True)
 
-# Checking missing data
-cartoon_data.isna().sum()
+cartoon_data.rename(columns = {'Unnamed: 0':'index'}, inplace = True)
 
-# 45 rows have missing data. We could fill up the rows or drop them. I chose to fill the rows with accurate data from the internet
-
-# Drop rows with shows that don't have end dates
-
-cartoon_data = cartoon_data.drop(labels=[12, 74, 79, 83, 84, 98, 89, 122, 124, 140, 199, 219], axis=0)
-
-# Create a list and fill the remaining na rows with its values
-
-List = [2002, 2003, 1980, 1980, 1982, 2021, 1982, 2011, 2010, 2020, 2002, 2007, 2006, 2005, 2015, 2003, 2000, 1967,
-        1966, 1969, 2007, 2012, 2015, 2021, 2018, 2020, 2012, 1993, 2018, 2020, 2020, 2017, 2017]
-cartoon_data.loc[cartoon_data.end_date.isna(), 'end_date'] = List
 
 # combining all the 5 selected features
 combined_features = cartoon_data['cartoon_names'] + ' ' + cartoon_data['release_date'].apply(str) + ' ' + cartoon_data[
@@ -38,12 +24,13 @@ def recommend(movie):
     movie_index = cartoon_data[cartoon_data['cartoon_names'] == movie].index[0]
     distances = similarity[movie_index]
     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+  #  print(distances)
 
     recommend_movies = []
 
     for i in movies_list:
-        recommend_movies.append(cartoon_data.iloc[i[0]][0])
-        print(recommend_movies)
+        recommend_movies.append(cartoon_data.iloc[i[0]][1])
+      #  print(recommend_movies)
 
     x1 = movies_list[0][0]  # starts here
     y1 = movies_list[0][1]
@@ -61,7 +48,8 @@ def recommend(movie):
     namex3 = cartoon_data['cartoon_names'].values[x3]
     namex4 = cartoon_data['cartoon_names'].values[x4]
     namex5 = cartoon_data['cartoon_names'].values[x5]
-    # cartoon_data[x1]
+
+
 
     plotdata = pd.DataFrame({"pies": [y1, y2, y3, y4, y5]},
                             index=[namex1, namex2, namex3, namex4, namex5])
@@ -80,7 +68,7 @@ def recommend(movie):
 
 st.title('Movie Recommender System')
 
-cartoon_data.head(100)["release_date"].plot()
+#cartoon_data.head(100)["release_date"].plot()
 
 selected_movie_name = st.selectbox('Please select a show from the menu below', cartoon_data['cartoon_names'].values)
 
